61c9bf54bbeeeafceba9e5a2680ddc193f429a6e
eggcat4501 <103723449+eggcat4501@users.noreply.github.com>
2022-09-11 09:30:45 +0300

    yes
    beeg code

---
+++ aa/yess.cs
@@ -0,0 +1,530 @@
+[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+sealed class ${yess}Attribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}argument sealed = (argument)
+(
+    attribute("go")
+    //how dose this even not show any errors i have a tiny bit of knowledge
+)
+AllowMultiple (arguments)
+(
+    at (0, 1, 5,)
+)
+[arguments/string] = [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+sealed class ${yess}Attribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }arguments{any
+    /(
+        arguments(All)
+    )}
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
+// w h a t? it still works?
+    arguments
+   (All)"yesy"
+   yessAttribute
+   [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+
+
+argument
+   //code lookin like E if you dont think so open this in vscode and look for the break points 
+   (
+
+
+    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class ${yess}Attribute : System.Attribute
+    {
+        // See the attribute guidelines at
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string positionalString;
+        
+        // This is a positional argument
+        public yessAttribute(string positionalString)
+        {
+            this.positionalString = positionalString;
+            
+            // TODO: Implement code here
+            throw new System.NotImplementedException();
+        }
+        
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+        
+        // This is a named argument
+        public int NamedInt { get; set; }
+    }
+  (arguments/any)[AttributeUsage"assert"]
+
+
+and
+
+
+    (
+
+
+
+
+    )
+  )and class yess
+  {
+    sealed
+  }
+   } private yess(521)
+   {
+    
+   }[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+   }[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+   }[System.AttributeUsage(System.AttributeTargets., Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+   }[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+   }[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+   }(
+
+    sealed
+    {and
+    return(and)
+    }
+   )[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+   }
+   (
+
+
+    attribute = "sus"
+    All.AllowMultiple a)
+
+    (
+
+
+
+  [assert]attribute = break
+   AttributeTargets.argument);
+   {[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+   sealed class ${yess}Attribute : System.Attribute
+   {
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+   }}AllowMultiple.arguments = (1, 2, 65,)
+   (
+
+at (assert)
+
+    attribute
+   )
+
+   guidelines.public int MyProperty { get; set; } public 
+   
+   
+   
+   
+    MyProperty { get; private set; }(
+
+
+
+        (
+         dose ([System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+         sealed class ${yess}Attribute : System.Attribute
+         {
+            // See the attribute guidelines at
+            //  http://go.microsoft.com/fwlink/?LinkId=85236
+            readonly string positionalString;
+            
+            // This is a positional argument
+            public yessAttribute(string positionalString)
+            {
+                this.positionalString = positionalString;
+                
+                // TODO: Implement code here
+                throw new System.NotImplementedException();
+            }
+            
+            public string PositionalString
+            {
+                get { return positionalString; }
+            }
+            
+            // This is a named argument
+            public int NamedInt { get; set; }
+         })
+
+
+        )
+             (
+
+
+                [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+                sealed class ${yess}Attribute : System.Attribute
+                {
+                    // See the attribute guidelines at
+                    //  http://go.microsoft.com/fwlink/?LinkId=85236
+                    readonly string positionalString;
+                    
+                    // This is a positional argument
+                    public yessAttribute(string positionalString)
+                    {
+                        this.positionalString = positionalString;
+         and(
+
+           {
+
+
+            assert.{#region assert
+                
+            #endregion}
+           }
+
+            sealed
+         )               
+                        // TODO: Implement code here
+                        throw new System.NotImplementedException();
+                    }
+                    
+                    public string PositionalString
+                    {
+                        get { return positionalString; }
+                    }
+                    
+                    // This is a named argument
+                    public int NamedInt { get; set; }
+                }
+             )
+
+        AllowMultiple.assert (
+
+
+
+
+            [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+            sealed class ${yess}Attribute : System.Attribute
+            {
+                // See the attribute guidelines at
+                //  http://go.microsoft.com/fwlink/?LinkId=85236
+                readonly string positionalString;
+                
+                // This is a positional argument
+                public yessAttribute(string positionalString)
+                {
+                    this.positionalString = positionalString;
+                    
+                    // TODO: Implement code here
+                    throw new System.NotImplementedException();
+                }
+                
+                public string PositionalString
+                {
+                    get { return positionalString; }
+                }
+                
+                // This is a named argument
+                public int NamedInt { get; set; }
+            }
+        )
+    )
+
+
+    [AttributeTargets] struct yess
+    {
+        arguments.Attribute
+
+
+        AllowMultiple.[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+        sealed class ${yess}Attribute : System.Attribute
+        {
+            // See the attribute guidelines at
+            //  http://go.microsoft.com/fwlink/?LinkId=85236
+            readonly string positionalString;
+            
+            // This is a positional argument
+            public yessAttribute(string positionalString)
+            {
+                this.positionalString = positionalString;
+                
+                // TODO: Implement code here
+                throw new System.NotImplementedException();
+            }
+            
+            public string PositionalString
+            {
+                get { return positionalString; }
+            }
+            
+            // This is a named argument
+            public int NamedInt { get; set; }
+        }
+    }
+
+
+<[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+sealed class ${yess}Attribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }>
+}
+
+(
+
+
+sealed
+switch (switch_off)
+{
+    
+    default:
+}
+
+    (arguments){1, 5, 1,}
+    arguments/assert
+)
+guidelines "assert"
+Attribute
+[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+sealed class ${yess}Attribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public yessAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+} and = attribute
